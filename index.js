ce63e25f965fb63194dfa7832c21677090eb39b3
𝐁𝐚𝐘𝐣𝐢𝐝 <aryanboss2@yahoo.com>
2025-07-16 13:11:08 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require("dotenv").config(); // .env file load
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
